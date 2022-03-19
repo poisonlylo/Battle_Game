f37f731f295b8cfd6c8063b8165d44037f328219
poisonlylo <tibichelyes@gmail.com>
2022-03-19 21:07:52 +0100

    mouse hendeler

--- Assets/scripts/helper Scripts/tagHolder.cs
+++ Assets/scripts/helper Scripts/tagHolder.cs
@@ -6,6 +6,8 @@ public class Axis
 {
     public const string HORIZENTAL = "Horizontal";
     public const string VERTICAL = "Vertical";
-
-
+}
+public class MouseAxis {
+    public const string MOUSE_X = "Mouse X";
+    public const string MOUSE_Y = "Mouse Y";
 }
